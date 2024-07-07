8dbb9674fb85e8ac779ea6e5312d86d811582098
Sagar Jha <sagarj.ext@shapoorji.com>
2024-07-07 16:32:15 +0530

    build project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
 export default defineConfig({
+  output: {
+    manualChunks(id) {
+      // Specify how modules should be grouped into chunks
+      if (id.includes('node_modules')) {
+        return 'vendor'; // Group all modules from node_modules into a 'vendor' chunk
+      } else {
+        return 'main'; // Group all other modules into a 'main' chunk
+      }
+    },
+    // other output options...
+  },
+
+  input: {
+    main: resolve(__dirname, 'index.html'),
+    404: resolve(__dirname, 'public/404.html'),
+  },
+
+  build: {
+    outDir: 'build',
+    chunkSizeWarningLimit: 5000, // Adjust the chunk size warning limit to 500 kBs
+    rollupOptions: {},
+    // You need to change this between build runs to reproduce
+    chunkFileNames: `assets/[name]-[hash].js`,
+    entryFileNames: `assets/[name]-[hash].js`,
+  },
+
   plugins: [react()],
-})
+});
